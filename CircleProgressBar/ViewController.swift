53dea41676f51ef74bad2b608ee85ab2a6313130
wwCondor <w.p.willebrands@hotmail.com>
2020-02-20 18:44:46 +0100

    Initial Commit

---
+++ CircleProgressBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleProgressBar
+//
+//  Created by Wouter Willebrands on 20/02/2020.
+//  Copyright © 2020 CodingCondor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
