197572a10b3c119909067b469d1d493d9cdb532a
wwCondor <w.p.willebrands@hotmail.com>
2020-02-21 10:33:57 +0100

    CircleProgressDisc first init
    A circle that could be used to inform user of a progress or loading status.

--- CircleProgressBar/ViewController.swift
+++ CircleProgressBar/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let shapeLayer = CAShapeLayer()
+    private let trackLayer = CAShapeLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
+        
+        configureTrack()
+        configureCircle()
+        
+        addGestureRecognizer()
     }
 
-
+    private func configureCircle() {
+        let center = view.center
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.systemPink.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = .round
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func configureTrack() {
+        let center = view.center
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.systemYellow.cgColor
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.lineCap = .round
+        
+        view.layer.addSublayer(trackLayer)
+    }
+    
+    private func addGestureRecognizer() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewTapped)))
+    }
+    
+    @objc private func viewTapped() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "basic")
+    }
 }
 
